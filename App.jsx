167dd878000684a5653c1ebe61daf3d8f5be622d
jghafoor06-coder <jghafoor06@gmail.com>
2026-05-05 02:01:43 -0700

    Initial commit

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import LoginScreen from './screens/LoginScreen'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <LoginScreen />
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
---
+++ screens/LoginScreen.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const LoginScreen = () => {
+  return (
+    <View>
+      <Text style={{color: "white", padding: 20, textAlign: "center", justifyContent: "center"}}>LoginScreen</Text>
+    </View>
+  )
+}
+
+export default LoginScreen;
